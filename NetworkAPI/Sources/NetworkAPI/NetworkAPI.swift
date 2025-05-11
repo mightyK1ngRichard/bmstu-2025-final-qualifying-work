bf3636526753cd445c2b51bf6176f0ef593ce18d
Dmitriy Permyakov <permyakov.d5@rwb.ru>
2025-05-11 04:58:03 +0300

    конфигурации. рефакторинг модулей

--- NetworkAPI/Sources/NetworkAPI/NetworkAPI.swift
+++
@@ -1,7 +0,0 @@
-//
-//  NetworkAPI.swift
-//  NetworkAPI
-//
-//  Created by Dmitriy Permyakov on 15.02.2025.
-//
-
---
+++ Tорт&Land/Modules/NetworkAPI/NetworkAPI.h
@@ -0,0 +1,19 @@
+//
+//  NetworkAPI.h
+//  NetworkAPI
+//
+//  Created by Dmitriy Permyakov on 11.05.2025.
+//  Copyright © 2025 https://github.com/mightyK1ngRichard. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NetworkAPI.
+FOUNDATION_EXPORT double NetworkAPIVersionNumber;
+
+//! Project version string for NetworkAPI.
+FOUNDATION_EXPORT const unsigned char NetworkAPIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NetworkAPI/PublicHeader.h>
+
+
